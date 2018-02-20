f1af2ca2d7a1ed7de5c5e9fb19f30091670db770
Emili Parreno <emili@eparreno.com>
2018-02-20 15:35:11 +0100

    Fix syntanx in ruby consumer

--- ruby/consumer.rb
+++ ruby/consumer.rb
@@ -1,8 +1,8 @@
-require 'avro'
+require "avro"
 require "kafka"
 
-KAFKA_HOST = 'localhost:9092'
-KAFKA_TOPIC = 'avro'
+KAFKA_HOST = "localhost:9092"
+KAFKA_TOPIC = "avro"
 
 schema = Avro::Schema.parse(File.open("../schemas/user.avsc", "rb").read)
 
@@ -11,13 +11,13 @@ consumer = kafka.consumer(group_id: "avro-test")
 consumer.subscribe(KAFKA_TOPIC, start_from_beginning: false)
 
 consumer.each_message do |message|
-	begin
-		buffer = StringIO.new(message.value)
-		decoder = Avro::IO::BinaryDecoder.new(buffer)
-		datumreader = Avro::IO::DatumReader.new(schema)
-		decoded_message = datumreader.read(decoder)
+  begin
+    buffer = StringIO.new(message.value)
+    decoder = Avro::IO::BinaryDecoder.new(buffer)
+    datumreader = Avro::IO::DatumReader.new(schema)
+    decoded_message = datumreader.read(decoder)
     puts "Message processed at #{Time.now.strftime("%Y-%m-%d %H:%M")}"
-		puts decoded_message
+    puts decoded_message
   rescue
     puts "Can't process message"
   end
--- ruby/producer.rb
+++ ruby/producer.rb
@@ -1,8 +1,8 @@
-require 'avro'
+require "avro"
 require "kafka"
 
-KAFKA_HOST = 'localhost:9092'
-KAFKA_TOPIC = 'avro'
+KAFKA_HOST = "localhost:9092"
+KAFKA_TOPIC = "avro"
 
 kafka = Kafka.new(seed_brokers: [KAFKA_HOST])
 schema = Avro::Schema.parse(File.open("../schemas/user.avsc", "rb").read)
@@ -12,11 +12,11 @@ def encode(schema)
   buffer = StringIO.new("".force_encoding("BINARY"))
   encoder = Avro::IO::BinaryEncoder.new(buffer)
 
-  # use old hash style, won't work otherwise
-  datum =  {"created_at" => "2015-10-21T09:47:50-04:00", "name" => "John", "favorite_color" => "red", "favorite_number" => 1 }
+  # use old hash style, won"t work otherwise
+  datum = { "created_at" => "2015-10-21T09:47:50-04:00", "name" => "John", "favorite_color" => "red", "favorite_number" => 1 }
 
   writer.write(datum, encoder)
-  return buffer.string
+  buffer.string
 end
 
 kafka.deliver_message(encode(schema), topic: KAFKA_TOPIC)
